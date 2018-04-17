248f2a88cc23d5329669bd9b4200b36723e197a6
Steve Bagwell <baggerone@gmail.com>
2018-04-17 08:34:31 -0400

    Added IsValidMACAddress function

--- domain.go
+++ domain.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"log"
 	"os"
+	"regexp"
 )
 
 const TagTable = "tagTable"
@@ -88,4 +89,19 @@ func ClientError(status int, body string) (events.APIGatewayProxyResponse, error
 // GetTableName returns the env var value of the string passed in
 func GetDbTableName(table string) string {
 	return os.Getenv(table)
+}
+
+// IsValidMacAddress checks whether the input is ...
+//   - 12 hexacedimal digits OR
+//   - 6 pairs of hexadecimal digits separated by colons and/or hyphens
+func IsValidMACAddress(mAddr string) bool {
+	controller := "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$"
+	match, _ := regexp.MatchString(controller, mAddr)
+
+	// no separators
+	if ! match {
+		match, _ = regexp.MatchString("^([0-9A-Fa-f]{12})$", mAddr)
+	}
+
+	return match
 }
