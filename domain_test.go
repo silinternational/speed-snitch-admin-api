5578fc7b08438f8fda5ebe340e986368d5ff9a29
Steve Bagwell <baggerone@gmail.com>
2018-04-18 08:35:49 -0400

    Initial commit - tests for CleanMacAddress

---
+++ domain_test.go
@@ -0,0 +1,57 @@
+package domain
+
+import "testing"
+
+type testMACAddr struct {
+	MA1 string
+	MA2 string
+	ExpectError bool
+}
+
+func TestCleanMACAddress(t *testing.T) {
+
+	testMAs := []testMACAddr{
+		{
+			MA1: "112233AAbbCC",
+			MA2: "112233aabbcc",
+			ExpectError: false,
+		},
+		{
+			MA1: "11:22-33:AA-bb:CC",
+			MA2: "11:22-33:aa-bb:cc",
+			ExpectError: false,
+		},
+		{
+			MA1: "112233AAbb",
+			MA2: "",
+			ExpectError: true, // too short
+		},
+		{
+			MA1: "112233AAbbCCDD",
+			MA2: "",
+			ExpectError: true, // too long
+		},
+		{
+			MA1: "G12233AAbbCC",
+			MA2: "",
+			ExpectError: true, // bad letter
+		},
+		{
+			MA1: "11:22-33aabbcc",
+			MA2: "",
+			ExpectError: true, // not enough delimiters
+		},
+	}
+
+	for _, ma := range testMAs {
+
+		resultMA, err := CleanMACAddress(ma.MA1)
+		if ma.ExpectError && err == nil {
+			t.Errorf("For test MAC Address %s expected an error but did not get one.", ma.MA1)
+		}
+
+		if resultMA != ma.MA2 {
+			t.Errorf(`For test MAC Address %s expected: "%s", but got "%s".`, ma.MA1, ma.MA2, resultMA)
+		}
+	}
+}
