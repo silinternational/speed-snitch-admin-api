23fcf09c08ab96dca8ec6e2297f5692e6e7c9bee
Phillip Shipley <phillip.shipley@gmail.com>
2018-05-15 10:50:19 -0400

    add script to use newer go version in codeship

--- codeship/setup.sh
+++ codeship/setup.sh
@@ -6,5 +6,9 @@ set -e
 npm install -g serverless
 npm install
 
+# Install newer Go
+export GO_VERSION=1.10.2
+source /dev/stdin <<< "$(curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/languages/go.sh)"
+
 go get -u github.com/golang/dep/cmd/dep
 dep ensure
