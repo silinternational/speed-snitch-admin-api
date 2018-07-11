e6e1a3ff9cef18c1ad6959a685e9ddbc65b84b31
Phillip Shipley <phillip.shipley@gmail.com>
2018-07-11 10:29:24 -0400

    added lambda function for running database migrations on yearly schedule

---
+++ cron/migrations/main.go
@@ -0,0 +1,26 @@
+package migrations
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/silinternational/speed-snitch-admin-api/db"
+	"os"
+)
+
+func handler(ctx context.Context, event events.CloudWatchEvent) error {
+	fmt.Fprintf(os.Stdout, "Starting database auto migrations")
+
+	err := db.AutoMigrateTables()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Error migrating database: %s", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
