6254ab4a61da4a5e3d0bf40d7df119d8c180a743
Steve Bagwell <baggerone@gmail.com>
2018-08-24 15:47:44 -0400

    Start code to send emails for MIA nodes

--- cron/alerts/main.go
+++ cron/alerts/main.go
@@ -4,14 +4,19 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/silinternational/speed-snitch-admin-api"
 	"github.com/silinternational/speed-snitch-admin-api/db"
+
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
 	"log"
 )
 
-type SnapshotConfig struct {
+type AlertsConfig struct {
 	DaysMissing int `json:"DaysMissing"`
 }
 
-func handler(config SnapshotConfig) []domain.Node {
+func handler(config AlertsConfig) []domain.Node {
 	log.Println("Starting Alert for MIA Nodes")
 
 	// Set default number of past days to process to 7 if not already set in config
@@ -25,9 +30,79 @@ func handler(config SnapshotConfig) []domain.Node {
 		return []domain.Node{}
 	}
 
-	log.Printf("%v MIA nodes found", len(nodes))
+	superAdmins := []domain.User{}
+	err = db.ListItems(&superAdmins, "")
+	if err != nil {
+		log.Println("Error getting list of SuperAdmin users: " + err.Error())
+		return []domain.Node{}
+	}
 
-	return nodes
+	msg := fmt.Sprintf("The following nodes have been MIA for more than %d day(s).", config.DaysMissing)
+	scheduledNodes := []domain.Node{}
+
+	for _, node := range nodes {
+		if node.IsScheduled() {
+			msg = fmt.Sprintf("%s\n%s", node.Nickname)
+			scheduledNodes = append(scheduledNodes, node)
+		}
+	}
+
+	if len(scheduledNodes) < 1 {
+		log.Print("No MIA nodes found")
+		return scheduledNodes
+	}
+
+	charSet := "UTF-8"
+
+	subject := "MIA Speedsnitch Nodes"
+	subjContent := ses.Content{
+		Charset: &charSet,
+		Data:    &subject,
+	}
+
+	msgContent := ses.Content{
+		Charset: &charSet,
+		Data:    &msg,
+	}
+
+	msgBody := ses.Body{
+		Text: &msgContent,
+	}
+
+	recipients := []*string{}
+	for _, admin := range superAdmins {
+		recipients = append(recipients, aws.String(admin.Email))
+	}
+
+	emailMsg := ses.Message{}
+	emailMsg.SetSubject(&subjContent)
+	emailMsg.SetBody(&msgBody)
+
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			ToAddresses: recipients,
+		},
+		Message: &emailMsg,
+		Source:  aws.String("no_reply@sil.org"),
+	}
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+
+	// Create an SES session.
+	svc := ses.New(sess)
+	result, err := svc.SendEmail(input)
+	if err != nil {
+		log.Println("Error sending MIA nodes email to superAdmins: " + err.Error())
+		return []domain.Node{}
+	}
+
+	log.Printf("%v MIA nodes found\n", len(nodes))
+	log.Printf("%v MIA node email sent to %v superAdmins\n", len(nodes), len(recipients))
+	log.Println(result)
+
+	return scheduledNodes
 }
 
 func main() {
