28238cfba2d4c3bc0c96e185e727b609011ed556
Unknown <baggerone@gmail.com>
2019-03-05 13:28:44 -0500

    build admin package without listing all the go files

--- codeship/build.sh
+++ codeship/build.sh
@@ -9,15 +9,7 @@ set -x
 # Build all the things
 go build -ldflags="-s -w" -o bin/config                     api/agent/config/main.go
 go build -ldflags="-s -w" -o bin/hello                      api/agent/hello/main.go
-go build -ldflags="-s -w" -o bin/admin                      api/admin/namedserver.go \
-                                                            api/admin/node.go \
-                                                            api/admin/report.go \
-                                                            api/admin/reportingevent.go \
-                                                            api/admin/speedtestnetserver.go \
-                                                            api/admin/tag.go \
-                                                            api/admin/user.go \
-                                                            api/admin/version.go \
-                                                            api/admin/main.go
+go build -ldflags="-s -w" -o bin/admin                      api/admin
 go build -ldflags="-s -w" -o bin/speedtestnetserverupdate   cron/speedtestnetserverupdate/main.go
 go build -ldflags="-s -w" -o bin/alerts                     cron/alerts/main.go
 go build -ldflags="-s -w" -o bin/dailysnapshot              cron/dailysnapshot/main.go
